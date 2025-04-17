f0c5b005d0b7c10ceb5262cf8926762f7539768c
SK <gfawkes1832@gmail.com>
2025-04-17 13:22:02 +0530

    Setup stuff + cards frontend

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,8 @@ import { type NextRequest } from 'next/server'
 import { updateSession } from '@/utils/middleware'
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request)
+    // UNCOMMENT THIS TO START AUTH MIDDLEWARE
+//   return await updateSession(request) 
 }
 
 export const config = {
